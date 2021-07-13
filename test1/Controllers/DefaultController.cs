c3a5111a2b9d6ce882c6e65626a77aeb82f070fc
hades89110 <s89110@gmail.com>
2021-07-13 21:07:22 +0800

    更新頁面、登入頁面修改

--- test1/Controllers/DefaultController.cs
+++ test1/Controllers/DefaultController.cs
@@ -12,9 +12,12 @@ namespace test1.Controllers
     public class DefaultController : Controller
     {
 
+
+        MyDataBase db = new MyDataBase();
         string connString = "server=127.0.0.1;port=3306;user id=qqq;password=123;database=dbtest;charset=utf8;";
         MySqlConnection conn = new MySqlConnection();
 
+        Boolean login = false;
 
 
         public ActionResult Index()
@@ -36,9 +39,21 @@ namespace test1.Controllers
                 conn.ConnectionString = connString;
                 if (conn.State != ConnectionState.Open)
                     conn.Open();
-
-                string id = "0";
-                string sql = @"INSERT INTO `edata` (`id`, `account`, `password`, `name`, `job`)
+                //自己找資料庫最大id+1設為id
+                int id=0;
+                string sql = @"SELECT MAX(id)max FROM`edata`";
+                try
+                {
+                    MySqlCommand cmd2 = new MySqlCommand(sql, conn);
+                    id = (int)cmd2.ExecuteScalar();
+                    id++;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                //string id = "0";
+                sql = @"INSERT INTO `edata` (`id`, `account`, `password`, `name`, `job`)    
                                 VALUES (@id, @account, @password, @name, @job)";
 
                 MySqlCommand cmd = new MySqlCommand(sql, conn);
@@ -52,14 +67,39 @@ namespace test1.Controllers
                 int index = cmd.ExecuteNonQuery();
                 conn.Close();
 
-               ViewBag.Msg = "註冊成功";
-                return View(data);
+                Response.Redirect("~/Default/Login");
+                return new EmptyResult();
             }
         }
 
-        
+        public ActionResult Login()
+        {
+            return View();
+        }
 
+        public ActionResult Update()
+        {
+            return View();
+        }
 
+        [HttpPost]
+        public ActionResult Login(FormCollection post)
+        {
+            string account = post["account"];
+            string password = post["password"];
+
+            if (db.CheckUserData(account, password))
+            {
+
+                Response.Redirect("~/Default/Update");
+                return new EmptyResult();
+            }
+            else
+            {
+                ViewBag.Msg = "登入失敗";
+                return View();
+            }
+        }
 
 
 
--- test1/Models/product.cs
+++ test1/Models/product.cs
@@ -11,7 +11,6 @@ namespace test1.Models
 
     public class UserData
     {
-        public string id { get; set; }
         public string account { get; set; }
         public string password1 { get; set; }
         public string password2 { get; set; }
@@ -22,7 +21,39 @@ namespace test1.Models
     public class MyDataBase
     {
 
-        
-    }
+        public bool CheckUserData(string account, string password)
+        {
 
+            string connString = "server=127.0.0.1;port=3306;user id=qqq;password=123;database=dbtest;charset=utf8;";
+            MySqlConnection conn = new MySqlConnection();
+            try
+            {
+                conn.ConnectionString = connString;
+                if (conn.State != ConnectionState.Open)
+                    conn.Open();
+
+                //Connect();
+                string strSQL = "SELECT 1 FROM `edata` WHERE `account` = '" + account + "' AND `password` = '" + password + "';";
+                MySqlCommand cmd = new MySqlCommand(strSQL, conn);
+
+                using (MySqlDataReader reader = cmd.ExecuteReader())
+                {
+                    if (reader.Read())
+                        return true;
+                }
+                return false;
+            }
+            catch (Exception ex)
+            {
+                string error = ex.ToString();
+                return false;
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+        }
+
+    }
 }
